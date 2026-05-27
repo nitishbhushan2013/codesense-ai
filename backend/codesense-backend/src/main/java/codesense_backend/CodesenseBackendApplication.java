82ebdec973e8002a2029e3b9480fc0fb34d5ab23
Nitish Bhushan <your-actual-email@gmail.com>
2026-05-27 22:54:20 +1000

    initial project setup - backend and frontend scaffolding

---
+++ backend/codesense-backend/src/main/java/codesense_backend/CodesenseBackendApplication.java
@@ -0,0 +1,13 @@
+package codesense_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesenseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/codesense-backend/src/test/java/codesense_backend/CodesenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package codesense_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
