0c8c974429bef4c10232275309b50efc790dc370
Nitish Bhushan <nitishbhushan2013@gmail.com>
2026-05-27 23:10:03 +1000

    fix: move main application class to correct package com.codesense

--- backend/codesense-backend/src/main/java/com/codesense/CodesenseBackendApplication.java
+++ backend/codesense-backend/src/main/java/com/codesense/CodesenseBackendApplication.java
@@ -1,13 +1,11 @@
-package codesense_backend;
+package com.codesense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodesenseBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CodesenseBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CodesenseBackendApplication.class, args);
+    }
 }
